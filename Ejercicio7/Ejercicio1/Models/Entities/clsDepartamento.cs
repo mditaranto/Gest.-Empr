3de034c2d3c0544c7d1aaa6ef097655e37088cea
mditaranto <mditaranto@iesnervion.es>
2023-11-09 08:25:57 +0100

    departamentos

--- Ejercicio7/Ejercicio1/Models/Entities/clsDepartamento.cs
+++ Ejercicio7/Ejercicio1/Models/Entities/clsDepartamento.cs
@@ -2,5 +2,18 @@
 {
     public class clsDepartamento
     {
+
+        private int idDepartamento;
+        private string nombreDepartamento;
+
+        public clsDepartamento() { }
+
+        public clsDepartamento(int idDepartamento, string nombreDepartamento)
+        {
+            this.idDepartamento = idDepartamento;
+            this.nombreDepartamento= nombreDepartamento;
+        }
+
+
     }
 }
