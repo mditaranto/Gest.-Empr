778a3d5e6ac362ac56c8510d42d00c3d8d76b73b
mditaranto <mditaranto@iesnervion.es>
2023-11-16 14:39:46 +0100

    Ejercicio1,2 y 3

---
+++ Tema8/Tema8/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Tema8.Models;
+
+namespace Tema8.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            ClsPersona persona = new ClsPersona("Matias", "Apellidos", 1, 1); 
+            return View(persona);
+        }
+
+        [HttpPost]
+        public IActionResult GuardarPersona(ClsPersona persona)
+        {
+            return View(persona);
+        }
+
+    }
+}
---
+++ Tema8/Tema8/Models/ClsPersona.cs
@@ -0,0 +1,62 @@
+﻿namespace Tema8.Models
+{
+    public class ClsPersona
+    {
+        #region atributos
+        private int id;
+        private String nombre;
+        private String apellidos;
+        private int idDept;
+        #endregion
+
+        #region constructores
+        public ClsPersona()
+        {
+            nombre = "";
+        }
+
+        public ClsPersona(String nombre, String apellidos, int idDept, int id)
+        {
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.idDept = idDept;
+            this.id = id;
+        }
+        #endregion
+
+        #region propiedades
+        public String Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public String Apellidos
+        {
+            get { return apellidos; }
+            set { apellidos = value; }
+        }
+        public int IdDept
+        {
+            get { return idDept; }
+            set { idDept = value; }
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public String Direccion { get; set; }
+
+        public String nombreCompleto
+        {
+            get { return $"Su nombre completo es: {nombre} {apellidos}"; }
+        }
+
+        public String FuncionNombreCompleto()
+        {
+            return $"Su nombre completo es: {nombre} {apellidos}";
+        }
+        #endregion
+    }
+}
