807ad6e3b93305f7d7187e4de282fa00c6fba44a
mditaranto <mditaranto@iesnervion.es>
2023-11-20 09:40:39 +0100

    Mandalorian terminado

--- Tema8/Mandalorian/Controllers/HomeController.cs
+++ Tema8/Mandalorian/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Mandalorian.Models;
+﻿using Mandalorian.DAL;
+using Mandalorian.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,28 +7,27 @@ namespace Mandalorian.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        /// <summary>
+        /// Devuelve la vista index
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
         {
-            List<Misiones> listaMisiones = new List<Misiones>();
+
             return View();
         }
+
+        /// <summary>
+        /// Recoge el id de la mision seleccionada y devuelve la vista detalles con la mision elegida
+        /// </summary>
+        /// <param name="mision"></param>
+        /// <returns></returns>
         [HttpPost]
         public IActionResult Detalles(Misiones mision)
         {
-            return View(mision);
+            Misiones misionElegida = ListaMisiones.getListaMisiones().Find(x => x.Id == mision.Id);
+            return View(misionElegida);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
--- Tema8/Mandalorian/DAL/ListaMisiones.cs
+++ Tema8/Mandalorian/DAL/ListaMisiones.cs
@@ -1,10 +1,16 @@
-﻿namespace Mandalorian.Models.ViewModels
+﻿using Mandalorian.Models;
+
+namespace Mandalorian.DAL
 {
-    static public class ListaMisiones
+    public static class ListaMisiones
     {
-        static public List<Misiones> getListaMisiones()
+        /// <summary>
+        /// Devuelve una lista de misiones
+        /// </summary>
+        /// <returns>lista</returns>
+        public static List<Misiones> getListaMisiones()
         {
-            return new List<Misiones>()
+            List<Misiones> lista = new List<Misiones>()
             {
                 new Misiones(1, "Rescate de Baby Yoda", "Debes hacerte con Grogu y llevárselo a Luke SkyWalker para su entrenamiento.", 5000),
                 new Misiones(2, "Recuperar armadura Beskar", "Tu armadura de Beskar ha sido robada. Debes encontrarla.", 2000),
@@ -12,6 +18,9 @@
                 new Misiones(4, "Renacuajos", "Debes llevar a una Dama Rana y sus huevos de Tatooine a la " +
                 "luna del estuario Trask, donde su esposo fertilizará los huevos.", 500)
             };
+
+            return lista;
+
         }
     }
 }
