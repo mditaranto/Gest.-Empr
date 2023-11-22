4a07fecb654b2d3bc2753670a49bb0a7c7a62ab2
mditaranto <mditaranto@iesnervion.es>
2023-11-22 14:39:44 +0100

    ALgo de BBDD

---
+++ Tema10/Ej1/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Ej1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.Data.SqlClient;
+
+namespace Ej1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+    }
+}
