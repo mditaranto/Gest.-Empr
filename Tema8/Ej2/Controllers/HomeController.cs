5f157d0579cf12b0b7e0f5ed99624a4b014ea11a
mditaranto <mditaranto@iesnervion.es>
2023-11-30 09:13:18 +0100

    sdads

---
+++ Tema8/Ej2/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Ej2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Tema9.Models;
+
+namespace Ej2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            ClsPersona persona = new ClsPersona("Matias", "Apellidos", 1, 1);
+            return View(persona);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(ClsPersona persona)
+        {
+            IActionResult action;
+            if (ModelState.IsValid)
+            {
+                action = RedirectToAction("GuardarPersona", persona);
+            }
+            else
+            {
+                action = View(persona);
+            }
+            return action;
+        }
+
+        [HttpPost]
+        public IActionResult GuardarPersona(ClsPersona persona)
+        {
+            return View(persona);
+        }
+
+    }
+}
---
+++ Tema8/Ej2/Models/ClsPersona.cs
@@ -0,0 +1,74 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tema9.Models
+{
+    public class ClsPersona
+    {
+
+        #region atributos
+        private int id;
+        private string nombre;
+        private string apellidos;
+        private int idDept;
+        #endregion
+
+        #region constructores
+        public ClsPersona()
+        {
+            nombre = "";
+        }
+
+        public ClsPersona(string nombre, string apellidos, int idDept, int id)
+        {
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.idDept = idDept;
+            this.id = id;
+        }
+        #endregion
+
+        #region propiedades
+
+        [Required(ErrorMessage = "El campo nombre es obligatorio")]
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        [Required(ErrorMessage = "El campo apellidos es obligatorio")]
+        [StringLength(10, ErrorMessage = "El campo apellidos no puede tener más de 10 caracteres")]
+        public string Apellidos
+        {
+            get { return apellidos; }
+            set { apellidos = value; }
+        }
+
+        public int IdDept
+        {
+            get { return idDept; }
+            set { idDept = value; }
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Direccion { get; set; }
+
+        public string nombreCompleto
+        {
+            get { return $"Su nombre completo es: {nombre} {apellidos}"; }
+        }
+
+        public string FuncionNombreCompleto()
+        {
+            return $"Su nombre completo es: {nombre} {apellidos}";
+        }
+        #endregion
+
+    }
+
+}
+
